297cc8f3c0d67ad3bcbcde2326c903332b693ee7
jvr990d <jvelasc3@uncc.edu>
2023-12-08 10:23:41 -0500

    Cal

---
+++ itis3135/scripts/Calculator.js
@@ -0,0 +1,125 @@
+const calculator = document.querySelector('.calculator')
+const keys = calculator.querySelector('.calculator__keys')
+const display = document.querySelector('.calculator__display')
+
+const calculate = (n1, operator, n2) => {
+    let result = ''
+    console.log(n1, operator, n2)
+
+    if (operator === 'add') {
+        result = parseFloat(n1) + parseFloat(n2)
+    } else if (operator === 'subtract') {
+        result = parseFloat(n1) - parseFloat(n2)
+    } else if (operator === 'multiply') {
+        result = parseFloat(n1) * parseFloat(n2)
+    } else if (operator === 'divide') {
+        result = parseFloat(n1) / parseFloat(n2)
+    }
+    console.log(result)
+    return result
+}
+
+keys.addEventListener('click', e => {
+    if (e.target.matches('button')) {
+        const key = e.target
+        const action = key.dataset.action
+        const keyContent = key.textContent
+        const displayedNum = display.textContent
+        const previousKeyType = calculator.dataset.previousKeyType
+
+
+        if (!action) {
+            if (displayedNum === '0' || previousKeyType === 'operator' || previousKeyType === 'calculate') {
+                display.textContent = keyContent
+            } else {
+                display.textContent = displayedNum + keyContent
+            }
+            calculator.dataset.previousKeyType = 'number'
+            console.log('number key!')
+        }
+
+        
+
+        if (
+            action === 'add' ||
+            action === 'subtract' ||
+            action === 'multiply' ||
+            action === 'divide'
+        ) {
+
+            const firstValue = calculator.dataset.firstValue
+            const operator = calculator.dataset.operator
+            const secondValue = displayedNum
+      
+            if (
+              firstValue &&
+              operator &&
+              previousKeyType !== 'operator' &&
+              previousKeyType !== 'calculate'
+            ) {
+              const calcValue = calculate(firstValue, operator, secondValue)
+              display.textContent = calcValue
+              calculator.dataset.firstValue = calcValue
+            } else {
+              calculator.dataset.firstValue = displayedNum
+            }
+            calculator.dataset.previousKeyType = 'operator'
+            calculator.dataset.operator = action
+            console.log('operator key!')
+
+        }
+
+        if (action === 'decimal') {
+            if (!displayedNum.includes('.')) {
+                display.textContent = displayedNum + '.'
+              } else if (
+                previousKeyType === 'operator' ||
+                previousKeyType === 'calculate'
+              ) {
+                display.textContent = '0.'
+              }
+        
+              calculator.dataset.previousKeyType = 'decimal'
+            console.log('decimal key!')
+        }
+
+        if (action === 'clear') {
+            if (key.textContent === 'AC') {
+                calculator.dataset.firstValue = ''
+                calculator.dataset.modValue = ''
+                calculator.dataset.operator = ''
+                calculator.dataset.previousKeyType = ''
+              } else {
+                key.textContent = 'AC'
+              }
+        
+              display.textContent = 0
+              calculator.dataset.previousKeyType = 'clear'
+            console.log('clear key!')
+        }
+
+        if (action !== 'clear') {
+            const clearButton = calculator.querySelector('[data-action=clear]')
+            clearButton.textContent = 'CE'
+          }
+
+        if (action === 'calculate') {
+            const firstValue = calculator.dataset.firstValue
+            const operator = calculator.dataset.operator
+            const secondValue = displayedNum
+
+            if (firstValue) {
+                if (previousKeyType === 'calculate') {
+                  firstValue = displayedNum
+                  secondValue = calculator.dataset.modValue
+                }
+        
+                display.textContent = calculate(firstValue, operator, secondValue)
+              }
+
+            calculator.dataset.modValue = secondValue
+            calculator.dataset.previousKeyType = 'calculate'
+            console.log('equal key!')
+        }
+    }
+})
